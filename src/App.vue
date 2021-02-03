b000f64961d513f78755c6784deb20d90b997afe
vmstr <marcroesner@googlemail.com>
2021-02-03 19:49:56 +0100

    update with vue cli

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header msg="Header" />
+    <StartView msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import StartView from './components/StartView.vue'
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    StartView
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w3-container w3-teal">
+      <h1>{{ info }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartView',
+  props: {
+    msg: String,
+  },
+  data: function () {
+    return {
+      info: 'Todo List'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
